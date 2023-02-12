e3bd309fb88d2ee451c9962f014d7007526456cb
gbubemi22 <slimnfine22@gmail.com>
2023-02-12 12:00:31 +0100

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import * as qrcode from 'qrcode';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  async getHello(): Promise<string> {
+    const link = `https://gist.github.com/saniyusuf/406b843afdfb9c6a86e25753fe2761f4`;
+    const qrCode = await qrcode.toDataURL(link);
+
+    return qrcode.toString(qrCode);
+  }
+}
